e155e728648e0ec647421d02aee2d9b66f02afa4
= <=>
2017-08-09 21:51:46 +0100

    com

--- Banana_or_Not/first_exploration.py
+++ Banana_or_Not/first_exploration.py
@@ -8,5 +8,5 @@ from tensorflow.examples.tutorials.mnist import input_data
 
 mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
 
-print("Number of data points to:\nTrain: {0}\nTest: {1}\nValidate: {2}".format(len(mnist.train.images), len(mnist.test.images), len(mnist.validation.images)))
-print(len(mnist.train.images))
+print("Number of data points to:\nTrain: {0}\nTest: {1}\nValidate: {2}"\
+        .format(len(mnist.train.images), len(mnist.test.images), len(mnist.validation.images)))
