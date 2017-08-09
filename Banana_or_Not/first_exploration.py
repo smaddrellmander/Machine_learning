dee2c7ad988b5fe6a64b46aa525ec09ba1f9627c
smaddrellmander <sam.maddrellmander@gmail.com>
2017-08-09 21:48:07 +0100

    Imports the MNIST data

---
+++ Banana_or_Not/first_exploration.py
@@ -0,0 +1,12 @@
+# A python script to start exploring MNIST
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import keras
+from tensorflow.examples.tutorials.mnist import input_data
+
+
+mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
+
+print("Number of data points to:\nTrain: {0}\nTest: {1}\nValidate: {2}".format(len(mnist.train.images), len(mnist.test.images), len(mnist.validation.images)))
+print(len(mnist.train.images))
