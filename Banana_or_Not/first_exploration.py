2d67a488571d99c68960a5616c8bc2bd13314b65
= <=>
2017-08-09 22:07:41 +0100

    Image generation

--- Banana_or_Not/first_exploration.py
+++ Banana_or_Not/first_exploration.py
@@ -10,3 +10,13 @@ mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
 
 print("Number of data points to:\nTrain: {0}\nTest: {1}\nValidate: {2}"\
         .format(len(mnist.train.images), len(mnist.test.images), len(mnist.validation.images)))
+
+def gen_image(arr):
+    two_d = (np.reshape(arr, (28, 28)) * 255).astype(np.uint8)
+    plt.imshow(two_d, interpolation='nearest')
+    return plt
+
+# Get a batch of two random images and show in a pop-up window.
+batch_xs, batch_ys = mnist.test.next_batch(2)
+gen_image(batch_xs[0]).show()
+gen_image(batch_xs[1]).show()
