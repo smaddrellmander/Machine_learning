667b98836a98eaa25dcd925de0594431d952d800
smaddrellmander <sam.maddrellmander@gmail.com>
2017-08-15 14:40:26 +0100

    Updates to saving and loading

--- Banana_or_Not/load_keras.py
+++ Banana_or_Not/load_keras.py
@@ -1,4 +1,3 @@
-# Basically the same as the example, but rewritten for understanding and neatness
 import keras
 from keras.datasets import mnist
 from keras.models import Sequential
@@ -7,6 +6,8 @@ from keras.layers import Conv2D, MaxPooling2D
 from keras import backend as K
 import matplotlib.pyplot as plt
 import numpy as np
+from keras.models import model_from_json
+
 
 def main():
     # Start with parameter values
@@ -50,27 +51,24 @@ def main():
     y_train = keras.utils.to_categorical(y_train, num_classes)
     y_test = keras.utils.to_categorical(y_test, num_classes)
 
-    model = Sequential()
-    model.add(Conv2D(32, kernel_size=(3,3),
-                     activation='relu',
-                     input_shape=input_shape))
-    model.add(Conv2D(64, (3,3), activation='relu'))
-    model.add(MaxPooling2D(pool_size=(2,2))) # Reducing size by factor 2^2
-    model.add(Dropout(0.25))
-    model.add(Flatten()) # To allow our normal NN to work on the vector
-    model.add(Dense(128, activation='relu'))
-    model.add(Dropout(0.5))
-    model.add(Dense(num_classes, activation='softmax')) # The output layer
+    # load json and create model
+    json_file = open('model.json', 'r')
+    loaded_model_json = json_file.read()
+    json_file.close()
+    loaded_model = model_from_json(loaded_model_json)
+    # load weights into new model
+    loaded_model.load_weights("model.h5")
+    print("Loaded model from disk")
 
-    model.compile(loss=keras.losses.categorical_crossentropy,
-                  optimizer = keras.optimizers.Adadelta(),
-                  metrics = ['accuracy'])
+    loaded_model.compile(loss=keras.losses.categorical_crossentropy,
+                         optimizer = keras.optimizers.Adadelta(),
+                         metrics = ['accuracy'])
 
-    logger = model.fit(x_train, y_train,
-                        batch_size = batch_size,
-                        epochs = epochs,
-                        verbose = 1,
-                        validation_data = (x_test, y_test))
+    logger = loaded_model.fit(x_train, y_train,
+                              batch_size = batch_size,
+                              epochs = epochs,
+                              verbose = 1,
+                              validation_data = (x_test, y_test))
 
     plt.plot(logger.history['loss'])
     plt.ylabel('loss')
@@ -78,24 +76,18 @@ def main():
     plt.show()
     plt.plot(logger.history['acc'])
     plt.ylabel('acc')
-    plt.xlabel('epoch')
+    ply.xlabel('epoch')
     plt.show()
-    score = model.evaluate(x_test, y_test, verbose = 0)
+    score = loaded_model.evaluate(x_test, y_test, verbose = 0)
     print('Test loss:', score[0])
     print('Test accuracy:', score[1])
 
     for N in Ns:
-        test = model.predict(x_val[N:N+1], batch_size=1)
+        test = loaded_model.predict(x_val[N:N+1], batch_size=1)
         print('Prediction:', np.argmax(test), 'Correct answer:', y_val[N])
         plt.imshow(x_vis[N])
         plt.show()
-    # serialize model to JSON
-    model_json = model.to_json()
-    with open("model.json", "w") as json_file:
-        json_file.write(model_json)
-    # serialize weights to HDF5
-    model.save_weights("model.h5")
-    print("Saved model to disk")
+
 
 
 if __name__ == '__main__':
