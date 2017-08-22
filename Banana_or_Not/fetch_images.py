ea563a036fb5512b91503928e39b2b4bcc98d351
= <=>
2017-08-22 21:20:27 +0100

    Big edits to picture fetching

--- Banana_or_Not/fetch_images.py
+++ Banana_or_Not/fetch_images.py
@@ -1,36 +1,30 @@
 import urllib3
 import shutil
-
-# TODO:
-# 1. Read in from file the URL list
-# 2. Rescale the images to correct size
-#
-#
-#
-
-
-url = 'https://is3-ssl.mzstatic.com/image/thumb/Purple30/v4/2d/86/b3/2d86b3bc-f6c2-8de7-3fa0-f64820e3926a/source/256x256bb.jpg'
-c = urllib3.PoolManager()
-filename = 'save.png'
-
-with c.request('GET',url, preload_content=False) as resp, open(filename, 'wb') as out_file:
-    shutil.copyfileobj(resp, out_file)
-
-resp.release_conn()
-
-
 import os, sys
 from PIL import Image
+import requests
+from io import StringIO
 
-infile = 'save.png'
-size = 32, 32
+from urllib import request
 
-
-outfile = os.path.splitext(infile)[0] + "dog.png"
-if infile != outfile:
+i = 0
+f = open('imagenet.synset.geturls.rooms.txt')
+for line in f:
+    i += 1
+    print('{0}/{1}'.format(i, 'len(f)'))
     try:
-        im = Image.open(infile)
-        im.thumbnail(size, Image.ANTIALIAS)
-        im.save(outfile)
-    except IOError:
-        print ("cannot create thumbnail for '%s'", infile)
+        img = request.urlretrieve(line, "banana.png")
+    except:
+        continue
+    infile = 'banana.png'
+    size = 32, 32
+
+
+    outfile = 'rooms/'+os.path.splitext(infile)[0] + "thumb"+str(i)+".png"
+    if infile != outfile:
+        try:
+            im = Image.open(infile)
+            im.thumbnail(size, Image.ANTIALIAS)
+            im.save(outfile)
+        except IOError:
+            print ("cannot create thumbnail for '{}'".format(infile))
