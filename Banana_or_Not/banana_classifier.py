1518e2a30a28d39591e954f781e403a84c2da85e
= <=>
2017-09-21 19:38:40 +0100

    Reworking how to load the iamges

--- Banana_or_Not/banana_classifier.py
+++ Banana_or_Not/banana_classifier.py
@@ -22,6 +22,7 @@ def get_data():
         a = img_to_array(img, data_format='channels_last')  # this is a Numpy array with shape (3, 150, 150)
         a = a.reshape((1,) + a.shape)
         bananas.append(a)
+    bananas = np.vstack(bananas)
     print(len(bananas))
     for fn in os.listdir('rooms/'):
         # if os.path.isfile(fn):
@@ -30,6 +31,7 @@ def get_data():
         a = img_to_array(img, data_format='channels_last')  # this is a Numpy array with shape (3, 150, 150)
         a = a.reshape((1,) + a.shape)
         rooms.append(a)
+    rooms = np.vstack(rooms)
     print(len(rooms))
     X_train = bananas[0:50]+rooms[0:50]
     X_test = bananas[50:100]+rooms[50:100]
@@ -39,31 +41,31 @@ def get_data():
     pass
 
 def main():
-    # get_data()
-    # X_train, y_train, X_test, y_test = get_data()
+    get_data()
+    X_train, y_train, X_test, y_test = get_data()
+
+    print (X_train)
+    print (y_train)
+
+    print(X_test)
+    print(y_test)
+
+    # # First attempt at using the ImageDataGenerator
+    # train_datagen = ImageDataGenerator(rescale=1./255)
+    # test_datagen = ImageDataGenerator(rescale=1./255)
     #
-    # print(X_train.shape)
-    # print(y_train.shape)
+    # train_generator = train_datagen.flow_from_directory(
+    #                 'train',
+    #                 target_size=(32,32),
+    #                 batch_size=32,
+    #                 class_mode='binary')
+    # test_generator = test_datagen.flow_from_directory(
+    #                 'test',
+    #                 target_size=(32,32),
+    #                 batch_size=32,
+    #                 class_mode='binary')
     #
-    # print(X_test.shape)
-    # print(y_test.shape)
-
-    # First attempt at using the ImageDataGenerator
-    train_datagen = ImageDataGenerator(rescale=1./255)
-    test_datagen = ImageDataGenerator(rescale=1./255)
-
-    train_generator = train_datagen.flow_from_directory(
-                    'train',
-                    target_size=(32,32),
-                    batch_size=32,
-                    class_mode='binary')
-    test_generator = test_datagen.flow_from_directory(
-                    'test',
-                    target_size=(32,32),
-                    batch_size=32,
-                    class_mode='binary')
-
-
+    # print(train_generator)
     # Show some of the data
     cols = 10
     rows = 5
@@ -72,17 +74,17 @@ def main():
     input_shape = (32, 32, 3)
     num_classes = 2
 
-    # fig = plt.figure(figsize=(2 * cols - 1, 2.5 * rows - 1))
-    # for i in range(cols):
-    #     for j in range(rows):
-    #         k = np.random.randint(0, X_train.shape[0])
-    #
-    #         ax = fig.add_subplot(rows, cols, i*rows+j+1)
-    #         ax.grid('off')
-    #         ax.axis('off')
-    #         ax.set_title('%s' % (class_names[np.where(y_train[k] > 0.0)[0][0]]))
-    #         im = ax.imshow(X_train[k])
-    # # plt.show()
+    fig = plt.figure(figsize=(2 * cols - 1, 2.5 * rows - 1))
+    for i in range(cols):
+        for j in range(rows):
+            k = np.random.randint(0, X_train.shape[0])
+
+            ax = fig.add_subplot(rows, cols, i*rows+j+1)
+            ax.grid('off')
+            ax.axis('off')
+            ax.set_title('%s' % (class_names[np.where(y_train[k] > 0.0)[0][0]]))
+            im = ax.imshow(X_train[k])
+    plt.show()
 
     model = Sequential()
     model.add(Conv2D(32, kernel_size=(3,3),
@@ -100,12 +102,11 @@ def main():
                   optimizer = keras.optimizers.Adadelta(),
                   metrics = ['accuracy'])
 
-    logger = model.fit_generator(train_generator,
-                        steps_per_epoch = 150,
+    logger = model.fit(X_train, y_train,
+                        batch_size = batch_size,
                         epochs = epochs,
                         verbose = 1,
-                        validation_data = test_generator,
-                        validation_steps = 100)
+                        validation_data = (X_test, y_test))
 
     y_predicted = model.predict(X_test)
     print('Accuracy:', np.mean( np.argmax(y_predicted, axis=1) ==  np.argmax(y_test, axis=1)))
