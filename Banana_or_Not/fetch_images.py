2cd54603001bdb410a00bfdb6be817eb0d3bad09
= <=>
2017-09-26 09:32:41 +0100

    Getting the poor images out

--- Banana_or_Not/fetch_images.py
+++ Banana_or_Not/fetch_images.py
@@ -8,7 +8,7 @@ from io import StringIO
 from urllib import request
 
 i = 0
-f = open('imagenet.synset.geturls.rooms.txt')
+f = open('imagenet.synset.geturls.txt')
 for line in f:
     i += 1
     print('{0}/{1}'.format(i, 'len(f)'))
@@ -17,7 +17,29 @@ for line in f:
     except:
         continue
     infile = 'banana.png'
-    size = 32, 32
+    size = 128, 128
+
+
+    outfile = 'rooms/'+os.path.splitext(infile)[0] + "thumb"+str(i)+".png"
+    if infile != outfile:
+        try:
+            im = Image.open(infile)
+            im = im.resize(size, Image.ANTIALIAS)
+            im.save(outfile)
+        except IOError:
+            print ("cannot create thumbnail for '{}'".format(len(infile)))
+
+i = 0
+f = open('imagenet.synset.geturls.rooms.txt')
+for line in f:
+    i += 1
+    print('{0}/{1}'.format(i, 'len(f)'))
+    try:
+        img = request.urlretrieve(line, "rooms.png")
+    except:
+        continue
+    infile = 'rooms.png'
+    size = 128, 128
 
 
     outfile = 'rooms/'+os.path.splitext(infile)[0] + "thumb"+str(i)+".png"
